41408af152b424abf8a660fbbb88acdc69f9696e
Walley Yang <walleyyang@gmail.com>
2019-05-25 21:38:17 -0400

    Completed like component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Like from "./common/like";
 import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
@@ -10,6 +11,14 @@ class Movies extends Component {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
     this.setState({ movies });
   };
+
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = {... movies[index]};
+    movies[index].liked = !movies[index].liked;
+    this.setState({movies});
+  }
 
   render() {
     const { length: count } = this.state.movies;
@@ -37,6 +46,7 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td><Like liked={movie.liked} onClick={() => this.handleLike(movie)} /></td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
